27a1a79370d8257501dd79c24079acc69daf1b84
poorvi shukla <shuklapoorvi26@gmail.com>
2024-03-07 20:34:02 +0530

    Add files via upload

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,29 @@
+public class InsertionSort {
+    public static void insertionSort(int arr[],int n){
+        for(int i=1;i<n;i++){
+            int temp=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>temp){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=temp;
+        }
+    }
+    public static void main(String args[]) {
+        int arr[] = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+        System.out.println("Before Using insertion Sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        insertionSort(arr, n);
+        System.out.println("After insertion sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    
+}
