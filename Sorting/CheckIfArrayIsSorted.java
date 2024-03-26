fd58f702013a0baa199d94659bc8f48d4fa13d30
poorvi shukla <shuklapoorvi26@gmail.com>
2024-03-26 20:20:13 +0530

    Add files via upload

--- Sorting/CheckIfArrayIsSorted.java
+++ Sorting/CheckIfArrayIsSorted.java
@@ -1,32 +1,17 @@
-// package Array40Questions;
+package RecursionQuestions;
 
 public class CheckIfArrayIsSorted {
-    // Brute force approach TIME--O(n^2) SPACE--O(1)
-    public static boolean isSorted(int arr[], int n) {
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[i])
-                    return false;
-            }
+    public static boolean isSorted(int arr[],int i){
+        if(i==arr.length-1){
+            return true;
         }
-        return true;
-    }
-
-    // Optimal approach TIME--O(n) SPACE--O(1)
-    public static boolean sorted(int arr[], int n) {
-        for (int i = 1; i < n; i++) {
-            if (arr[i] < arr[i - 1]) {
-                return false;
-            }
+        if(arr[i]>arr[i+1]){
+            return false;
         }
-        return true;
+        return isSorted(arr,i+1);
     }
-
-    //Driver code
     public static void main(String args[]){
-        int array[]={1,2,3,4,5};
-        System.out.println(isSorted(array, 5));
-        System.out.println(sorted(array, 5));
+        int arr[]={1,2,3,4,5};
+        System.out.println(isSorted(arr,0));
     }
-
 }
